daa964e51fa0040c3edbb948972cda4e859a5407
MykolaKozakov <niklot47@gmail.com>
2022-01-31 11:21:11 +0200

    master

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from "react";
 
-import Users from "./components/Users/Users";
-import UserDetails from "./components/UserDetails/UserDetails";
-import Posts from "./components/Posts/Posts";
 
 function App() {
     return (
-        <div className="App">
-            <div className='header'>
-                <Users/>
-                <UserDetails/>
-            </div>
-            <Posts/>
+        <div>
+
         </div>
     );
 }
